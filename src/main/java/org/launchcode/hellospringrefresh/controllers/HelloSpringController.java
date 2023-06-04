d830a2259d523c3fae08b2adbb337cd0afba9f40
Kyle Stadler <kstad4@gmail.com>
2023-06-04 17:29:00 -0500

    Finished Lesson 13.5

--- src/main/java/org/launchcode/hellospringrefresh/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospringrefresh/controllers/HelloSpringController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospringrefresh.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloSpringController {
@@ -11,10 +15,28 @@ public class HelloSpringController {
         return "form";
     };
 
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Jimmy Neutron");
+        names.add("Bill Nye");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 
